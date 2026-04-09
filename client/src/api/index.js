de941afe3107fcc5eb714789add254d2b997be64
Biku4321 <samantabikash83939@gmail.com>
2026-04-09 23:43:08 +0530

    fix: update api baseURL to live render url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const api = axios.create({ baseURL: "https://cleverbooks-backend.onrender.com/api" });
 
 export const uploadSettlements = (formData) => api.post("/settlements/upload", formData);
 export const getSettlements    = (status)   => api.get("/settlements", { params: { status } });
